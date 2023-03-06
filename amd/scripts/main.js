122660c748619fc5b74c7ab2f2fea7d08568aa48
Troy Taylor <troytaylor.net@gmail.com>
2023-03-06 15:56:32 -0500

    Initial commit

---
+++ amd/scripts/main.js
@@ -0,0 +1,15 @@
+﻿require.config({
+    paths: {
+        jquery: "http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min",
+        module1: "modules/module1",
+        module2: "modules/module2",
+        module3: "modules/module3",
+    }
+});
+
+require(["jquery", "module1"], function ($, mod1) {
+    
+    $("#clicker").on("click", function () {
+        mod1.go();
+    });
+});
---
+++ readme.txt
@@ -0,0 +1,2 @@
+Please begin with reading the 1-JavaScriptGettingStarted2013.pdf document. 
+It contains instructions on how to get started. 
